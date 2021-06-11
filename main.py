bb6d35a449688ec970c9f8fb8c30151f3746b66b
Mariia Balitskaia <mariiabalitskaia@Mariias-MacBook-Pro.local>
2021-06-11 11:52:10 +0700

    Some bugs fixed.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import datetime
 import json
 import logging
 import random
@@ -8,11 +7,11 @@ from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
 from authorization import bot_token
-from bot_responses import help_message, start_message
+from bot_responses import help_message, start_message, response_with_pv
 
 # Enable logging
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO
 )
 logger = logging.getLogger(__name__)
@@ -31,7 +30,7 @@ def help_command(update: Update, context: CallbackContext):
 
 class PhrasalVerb:
     def __init__(self):
-        with open('phrasal_verbs.json', 'r') as pv_file:
+        with open("phrasal_verbs.json", "r") as pv_file:
             verbs = json.load(pv_file)
 
         self.prepare = {str(f"{verb['verb']} {verb['prep']}"): [
@@ -39,42 +38,40 @@ class PhrasalVerb:
              'synonyms': f"{', '.join(verb['synonyms'])}",
              'examples': f"{';'.join(verb['examples'])}"}]
             for verb in verbs}
-        self.response = str()
+        self.pv = str()
 
     def send_start_message(self, update: Update, context: CallbackContext):
-        self.response = random.choice([key for key in self.prepare])
-        daily_response = f"Today's phrasal verb:  {self.response.capitalize()}." \
-                         f"\nClick -> /definition to get a definition;" \
-                         f"\nClick -> /synonyms to get a synonyms;" \
-                         f"\nClick -> /examples to get examples."
-        update.message.reply_text(daily_response)
+        self.pv = random.choice([key for key in self.prepare])
+        response = f"Today's phrasal verb:  {self.pv.capitalize()}{response_with_pv}."
+        update.message.reply_text(response)
 
     def pv_definition(self, update: Update, context: CallbackContext):
         update.message.reply_text(
-            f"Definition:\n{self.prepare[self.response][0]['definition'].capitalize()}.")
+            f"Definition:\n{self.prepare[self.pv][0]['definition'].capitalize()}.")
 
     def pv_synonyms(self, update: Update, context: CallbackContext):
         update.message.reply_text(
-            f"Synonyms:\n{self.prepare[self.response][0]['synonyms']}.")
+            f"Synonyms:\n{self.prepare[self.pv][0]['synonyms']}.")
 
     def pv_examples(self, update: Update, context: CallbackContext):
-        if self.prepare[self.response][0]['examples']:
+        if self.prepare[self.pv][0]['examples']:
             update.message.reply_text(
-                f"Examples:\n{self.prepare[self.response][0]['examples']}")
+                f"Examples:\n{self.prepare[self.pv][0]['examples']}")
         else:
             update.message.reply_text(f"Sorry, no examples for getting verb.")
 
 
-def main():
-    chat = PhrasalVerb()
+chat = PhrasalVerb()
 
+
+def main():
     updater = Updater(bot_token, use_context=True)
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
     dispatcher.add_handler(
-        CommandHandler("lets_start", chat.send_start_message))
+        CommandHandler("send_me_phrasal_verb", chat.send_start_message))
     dispatcher.add_handler(CommandHandler("definition", chat.pv_definition))
     dispatcher.add_handler(CommandHandler("synonyms", chat.pv_synonyms))
     dispatcher.add_handler(CommandHandler("examples", chat.pv_examples))
