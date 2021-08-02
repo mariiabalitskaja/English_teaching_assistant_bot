56c222e105b8f3aa495092a455eafa037d538b96
Mariia Balitskaia <mariiabalitskaia@Mariias-MacBook-Pro.local>
2021-08-02 14:16:33 +0300

    Separated branches

--- authorization.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env python3
-
-bot_token = "1747573180:AAGbOm5Rb050oOvoD4Yb6gS7MJhwhKP0Kwk"
--- bot_responses.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-help_message = "With me you can learn English phrasal verb!\nIf you want to start click /send_me_phrasal_verb."
-start_message = "Click /help to find out what I can\."
-response_with_pv = f"\nClick -> /definition to get a definition;" \
-                   f"\nClick -> /synonyms to get a synonyms;" \
-                   f"\nClick -> /examples to get examples."
